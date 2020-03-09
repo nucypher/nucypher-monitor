df4ce9eaa5b682b8964ddce3ad68db383ebe2bf6
derekpierre <derek.pierre@gmail.com>
2020-03-09 13:34:02 -0400

    Increase size of geolocation map

--- monitor/charts.py
+++ monitor/charts.py
@@ -190,7 +190,7 @@ def nodes_geolocation_map(nodes_dict: dict, ip2loc: IP2Location):
     graph = dcc.Graph(figure=fig,
                       id='nodes-geolocation',
                       config=GRAPH_CONFIG,
-                      style={'width': '100%'})
+                      style={'width': '100%', 'height': 750})
     return graph
 
 
